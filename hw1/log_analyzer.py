9c9a9908a2cd183e60ef40d739ccefce91910ef2
Alexandr.Zaytsev <Aleksandr.Zaytcev@gmail.com>
2017-11-05 02:06:42 +0300

    Реализовано разбор и сохранение лога

--- hw1/log_analyzer.py
+++ hw1/log_analyzer.py
@@ -6,17 +6,22 @@
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
-import argparse
+import collections
+import json
+from argparse import ArgumentParser
+from datetime import datetime
+import sys
 import gzip
 import glob
-import sys
 import re
-import datetime
+import os
+
+import math
 
 CONFIG = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log"
+    "LOG_DIR": "./Logs"
 }
 
 
@@ -30,7 +35,7 @@ def parse_args():
     $ ./log_analyzer.py -f json                     (сохранить в файле json)
     $ ./log_analyzer.py --log_path --format json    (путь к лог файлам + сохранение в файле json)
     """
-    parser = argparse.ArgumentParser(description='Log Analyzer')
+    parser = ArgumentParser(description='Log Analyzer')
     parser.add_argument('-l', '--log_path', help='Путь к лог файлам')
     parser.add_argument('-j', '--json', help='Сохранить в json файл', action='store_true')
     return parser.parse_args()
@@ -38,7 +43,8 @@ def parse_args():
 
 # region Чтение файла
 def get_latest_file(path):
-    files = glob.glob("{0}{1}".format(path, '/nginx-access-ui.log-*'))
+    """Получение файла с максимальной датой(последнего файла)"""
+    files = glob.glob("{0}{1}".format(path, '/nginx-access-ui.log-*.gz'))
     if files:
         latest_file = max(files, key=get_file_date)
         return latest_file
@@ -46,17 +52,15 @@ def get_latest_file(path):
 
 
 def get_file_date(path):
-    file_date = re.match(r'^.*-(\d+)\.?\w*$', path)
-    if file_date:
-        return datetime.strptime(file_date.group(1), '%Y%m%d')
+    """Получение даты из имени файла"""
+    date = re.search(r'[0-9]{8}', path).group(0)
+    if date:
+        return datetime.strptime(date, '%Y%m%d')
     raise RuntimeError('Не правильный формат именования лог файла')
 
 
-# endregion
-
-
-
 def read_file_by_rows(path):
+    """Чтение файла по строкам"""
     if path.endswith(".gz"):
         log = gzip.open(path, 'rb')
     else:
@@ -64,6 +68,100 @@ def read_file_by_rows(path):
     for line in log:
         yield line
     log.close()
+# endregion
+
+
+# region Анавлиз лог файла
+def get_report(log, total_time, limit=100):
+    """Получение отчета"""
+    report = []
+    one_percent = float(len(log) / 100.0)
+    one_time_percent = float(total_time / 100.0)
+
+    for url, times in log.items():
+        count = len(times)
+        time_sum = sum(times)
+        report.append({
+            'url': url,
+            'time_max': max(times),
+            'count': count,
+            'time_sum': round(time_sum, 3),
+            'count_perc': round(count / one_percent, 3),
+            'time_perc': round(time_sum / one_time_percent, 3),
+            'time_p50': round(percentile(times, 50), 3),
+            'time_p95': round(percentile(times, 95), 3),
+            'time_p99': round(percentile(times, 99), 3)
+        })
+    return report[:limit]
+
+
+def parse_line(line):
+    """Разбор строки"""
+    url = "-"
+
+    search_result = re.search(r'\s\/\S*', line)
+    if search_result:
+        url = search_result.group(0).rstrip()
+
+    return {
+        'url': url,
+        'request_time': float(re.search(r'[0-9]*.[0-9]*$', line).group(0)),
+    }
+
+
+def percentile(lst, p):
+    index = (p / 100.0) * len(lst)
+    if math.floor(index) == index:
+        result = (lst[int(index) - 1] + lst[int(index)]) / 2.0
+    else:
+        result = lst[int(math.floor(index))]
+    return result
+
+
+def log_analysis(path, is_json):
+    """Анализ файла лога"""
+    report_format = 'json' if is_json else 'html'
+    report_date = datetime.strftime(get_file_date(path), '%Y.%m.%d')
+    report_path = '{0}/report-{1}.{2}'.format(CONFIG['REPORT_DIR'], report_date, report_format)
+    if os.path.isfile(report_path):
+        print "Отчет {0} уже существует".format(report_path)
+        sys.exit(0)
+    print "Чтение {0} файла...".format(path)
+
+    total_time = 0
+    log = collections.defaultdict(list)
+    try:
+        for line in read_file_by_rows(path):
+            parsed_line = parse_line(line)
+            total_time += parsed_line['request_time']
+            log[parsed_line['url']].append(parsed_line['request_time'])
+        report = get_report(log, total_time, CONFIG['REPORT_SIZE'])
+        report_save(report, report_path)
+        print "Отчет сформирован в файле {0}".format(report_path)
+    except Exception as err:
+        print "Не корректные данные в лог файле {0}".format(path)
+        print str(err)
+
+# endregion
+
+
+# region Сохрание отчета
+
+def report_save(report, file_path):
+    """Сохранение отчета"""
+    if file_path.endswith('.html'):
+        with open('report.html', 'r') as f:
+            file_data = f.read()
+        file_data = file_data.replace('$table_json', json.dumps(report))
+        with open(file_path, 'w') as f:
+            f.write(file_data)
+    elif file_path.endswith('.json'):
+        with open(file_path, 'w') as f:
+            json.dump(report, f)
+    else:
+        raise RuntimeError('Unexpected report file format')
+
+# endregion
 
 
 def main():
@@ -75,7 +173,7 @@ def main():
 
     if log_path:
         try:
-            #  todo реализовать анлаиз лог файла
+            log_analysis(log_path, args.json)
         except Exception as err:
             print str(err)
             sys.exit(1)
@@ -84,4 +182,7 @@ def main():
 
 
 if __name__ == "__main__":
+    main()
+
+    sys.argv.append('-j')
     main()
